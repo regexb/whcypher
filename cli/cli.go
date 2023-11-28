d4b5782b64dd8b000959c201d3a6556639fb4721
Brian Egizi <brian.egizi@gmail.com>
2023-11-28 11:55:20 -0800

    Init commit

---
+++ cli/cli.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"log/slog"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/regexb/whcypher"
+	"github.com/urfave/cli/v2"
+)
+
+func loadSource(file string) ([][][]string, error) {
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	var out [][][]string
+	groups := bytes.Split(data, []byte("\n\n"))
+	for _, g := range groups {
+		var sub [][]string
+		lines := bytes.Split(g, []byte("\n"))
+		for _, l := range lines {
+			sub = append(sub, strings.Split(string(l), ""))
+		}
+		out = append(out, sub)
+	}
+	return out, nil
+}
+
+func cypherTreeFromSource(source [][][]string) (*whcypher.Trie, error) {
+	root := whcypher.NewNode("\000")
+	trie := &whcypher.Trie{RootNode: root}
+
+	for pi, page := range source {
+		for ri, row := range page {
+			if err := trie.InsertPageRow(pi, ri, row); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return trie, nil
+}
+
+func main() {
+	app := &cli.App{
+		Name: "whcli",
+		Flags: []cli.Flag{
+			&cli.PathFlag{Name: "file", Aliases: []string{"f"}},
+			&cli.StringFlag{Name: "input", Aliases: []string{"in", "i"}},
+			&cli.IntFlag{Name: "page_offset", Aliases: []string{"p"}, Value: 1},
+			&cli.IntFlag{Name: "row_offset", Aliases: []string{"r"}, Value: 1},
+			&cli.IntFlag{Name: "col_offset", Aliases: []string{"c"}, Value: 1},
+			&cli.BoolFlag{Name: "ltr", Value: false},
+		},
+		Action: func(ctx *cli.Context) error {
+			slog.Info("Starting...")
+
+			sourceFile := ctx.Path("file")
+			start := time.Now()
+			source, err := loadSource(sourceFile)
+			if err != nil {
+				slog.Error("Failed to load source", "file", sourceFile)
+				return err
+			}
+			slog.Info("Finished loading source", "pages", len(source), "time", time.Since(start))
+
+			slog.Info("Loading source into trie")
+			start = time.Now()
+			cypher, err := cypherTreeFromSource(source)
+			if err != nil {
+				slog.Error("Failed to load source into cypher trie", "time", time.Since(start))
+				return err
+			}
+			slog.Info("Finished loading source into cypher trie", "time", time.Since(start))
+
+			in := ctx.String("input")
+			start = time.Now()
+			var out [][4]int
+			if ctx.Bool("ltr") {
+				out, err = cypher.ConstructPhraseLTR(in)
+			} else {
+				out, err = cypher.ConstructPhraseLongest(in)
+			}
+			if err != nil {
+				slog.Info("Failed to generate cypher", "phrase", in, "time", time.Since(start))
+				return err
+			}
+			slog.Info("Finished generating cypher", slog.Any("raw", out), slog.Duration("time", time.Since(start)))
+
+			fmt.Fprintln(ctx.App.Writer, "Generated cypher:")
+			for i, part := range out {
+				fmt.Fprintf(ctx.App.Writer, "%d %d %d %d", part[0]+ctx.Int("page_offset"), part[1]+ctx.Int("row_offset"), part[2]+ctx.Int("col_offset"), part[3])
+				if i == len(out)-1 {
+					fmt.Fprintln(ctx.App.Writer)
+				} else {
+					fmt.Fprint(ctx.App.Writer, " ")
+				}
+			}
+
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ whcypher.go
@@ -0,0 +1,169 @@
+package whcypher
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+)
+
+type Node struct {
+	Char     string
+	Children [26]*Node
+	KnownLoc [][3]int // int[[page, row, col], [page, row, col]]
+}
+
+func (n *Node) AddLoc(page, row, col int) {
+	n.KnownLoc = append(n.KnownLoc, [3]int{page, row, col})
+}
+
+func NewNode(char string) *Node {
+	n := &Node{Char: char}
+	for i := 0; i < 26; i++ {
+		n.Children[i] = nil
+	}
+	return n
+}
+
+type Trie struct {
+	RootNode *Node
+}
+
+func (t *Trie) InsertPageRow(page, rowNum int, letters []string) error {
+	// Add up to 3 letters for each starting letter
+	for i := range letters {
+		next := letters[i:]
+		if err := t.InsertPagePart(page, rowNum, i, next); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (t *Trie) InsertPagePart(page, rowNum, colStart int, letters []string) error {
+	current := t.RootNode
+	for i, l := range letters {
+		l := strings.ToLower(string(l[0])) // ensure only 1 character
+		index := l[0] - 'a'                // 99 - lower ascii table decimal number
+		if index < 0 || index > 25 {
+			return fmt.Errorf("invalid characters in source: %q", l)
+		}
+		if current.Children[index] == nil {
+			current.Children[index] = NewNode(string(l))
+		}
+		current = current.Children[index]
+
+		// then add loc
+		if current != t.RootNode {
+			current.AddLoc(page, rowNum, colStart+i)
+		}
+	}
+	return nil
+}
+
+// SearchLetters returns the index of the term found up until and all the known locations.
+// If the whole term was found, the index will be len(term)
+func (t *Trie) SearchLetters(term string) (int, [][3]int) {
+	current := t.RootNode
+	strippedTerm := strings.ToLower(term)
+	for i := 0; i < len(strippedTerm); i++ {
+		index := strippedTerm[i] - 'a'
+
+		// next letter not found
+		if current == nil || current.Children[index] == nil {
+			return i, current.KnownLoc
+		}
+		current = current.Children[index]
+	}
+	return len(strippedTerm), current.KnownLoc
+}
+
+// ConstructPhraseLTR uses a left to right search to find the longest runs of
+// letters it can until the phrase is complete.
+func (t *Trie) ConstructPhraseLTR(phrase string) ([][4]int, error) {
+
+	phraseLocations := [][4]int{}
+
+	// Strip down phrase for searching.
+	strippedPhrase := strings.ToLower(strings.ReplaceAll(phrase, " ", ""))
+
+	// Use search until the phrase is complete.
+	remaining := strippedPhrase[0:]
+	for len(remaining) > 0 {
+		index, locations := t.SearchLetters(remaining)
+
+		if index < 1 || len(locations) < 1 {
+			return nil, fmt.Errorf("Letter %s not found", string(remaining[index]))
+		}
+		ri := rand.Intn(len(locations))
+		loc := [4]int{locations[ri][0], locations[ri][1], locations[ri][2] - index + 1, index}
+		phraseLocations = append(phraseLocations, loc)
+		remaining = remaining[index:]
+	}
+
+	return phraseLocations, nil
+}
+
+func (t *Trie) ConstructPhraseLongest(phrase string) ([][4]int, error) {
+	strippedPhrase := strings.ToLower(strings.ReplaceAll(phrase, " ", ""))
+	return t.findAllLongest(strippedPhrase)
+}
+
+func (t *Trie) findAllLongest(phrase string) (res [][4]int, err error) {
+	if len(phrase) == 0 {
+		return nil, fmt.Errorf("invalid phrase %q", phrase)
+	}
+
+	li, ls, lloc := t.FindLongest(phrase)
+	if len(lloc) == 0 {
+		return nil, fmt.Errorf("unable to complete phrase %q", phrase)
+	}
+
+	ri := rand.Intn(len(lloc))
+	lpart := [4]int{lloc[ri][0], lloc[ri][1], lloc[ri][2] - ls + 1, ls}
+
+	if len(phrase) == ls {
+		return append(res, lpart), nil
+	}
+
+	pre := phrase[0:li]
+	post := phrase[li+ls:]
+
+	// Prefix remaining
+	if len(pre) > 0 {
+		prer, prerr := t.findAllLongest(pre)
+		if prerr != nil {
+			return nil, prerr
+		}
+		res = append(res, prer...)
+	}
+
+	// Main
+	res = append(res, lpart)
+
+	// Postfix remaining
+	if len(post) > 0 {
+		posr, poerr := t.findAllLongest(post)
+		if poerr != nil {
+			return nil, poerr
+		}
+		res = append(res, posr...)
+	}
+
+	return
+}
+
+func (t *Trie) FindLongest(phrase string) (longestIndex int, longestSize int, longestLoc [][3]int) {
+	for i := 0; i < len(phrase); i++ {
+		check := phrase[i:]
+		s, loc := t.SearchLetters(check)
+		if s > longestSize {
+			longestIndex = i
+			longestLoc = loc
+			longestSize = s
+		}
+		if s > len(phrase)/2 {
+			return
+		}
+	}
+	return
+}
